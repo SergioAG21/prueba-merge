e1903466efc9f3dde29e9f52fb167d002f8805b6
SergioAG <sergioagdev@gmail.com>
2025-11-11 23:22:46 +0100

    Initial commit

---
+++ src/test/java/com/example/prueba_api/PruebaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
