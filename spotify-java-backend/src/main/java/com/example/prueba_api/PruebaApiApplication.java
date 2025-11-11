b52badd59642ffc019efd239680180ecc3b7fd66
SergioAG <sergioagdev@gmail.com>
2025-11-11 23:51:22 +0100

    Prueba

--- spotify-java-backend/src/main/java/com/example/prueba_api/PruebaApiApplication.java
+++ spotify-java-backend/src/main/java/com/example/prueba_api/PruebaApiApplication.java
@@ -8,6 +8,7 @@ public class PruebaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaApiApplication.class, args);
+        System.out.println("PruebaApiApplication started");
 	}
 
 }
